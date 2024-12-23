9ab205d45e0e4f50c5aa58c55847a45cba095d86
Ritesh Singh <yesfirst000@gmail.com>
2024-12-23 20:36:59 +0545

    verify-email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -20,3 +20,23 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`)
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{email}];
+
+    try {
+      const response =   await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "20d28943-b761-4aa0-aea0-037536e0d2e3",
+            template_variables: {
+                company_info_name: "Auth-Project",
+                name: name,
+              }
+        })
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+        throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
